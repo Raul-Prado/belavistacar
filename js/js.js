61776abc426abc66ed8216c2f08945b1a18a8402
Raul-Prado <73708236+Raul-Prado@users.noreply.github.com>
2022-07-02 18:17:23 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,26 +1,19 @@
 $(document).ready(function()
 {
-    // $('.carousel').carousel();
-    // $('.carousel.carousel-slider').carousel({
-    //     fullWidth: true,
-    //     indicators: true,
-    //     transition: 700,
-    //     interval: 8550,
-    //     height: 430
-    // });
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        height: 430
+    });
+    // setInterval(function() {
+ 
+    //     $('.carousel.carousel-slider').carousel('next');
+     
+    // }, 8000);
     $('.scrollspy').scrollSpy();
     // zoom foto
     $('.materialboxed').materialbox();
     //mostrar munu mobile
     $('.button-collapse').sideNav();
-    // slider
-    $('.slider').slider({
-        fullWidth: true,
-        indicators: false,
-        // transition: 700,
-        // interval: 8550,
-        height: 550
-    });
     // parallax
     $('.parallax').parallax();
     // tooltipped
